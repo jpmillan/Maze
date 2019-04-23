585c359d7fd5b529716b0a4e2f6117a22d1faccd
jpmillan <jpmillan@gmail.com>
2019-04-23 16:37:20 +1000

    add content for step class

--- src/main/java/com/jpmillan/maze/Step.java
+++ src/main/java/com/jpmillan/maze/Step.java
@@ -1,4 +1,33 @@
 package com.jpmillan.maze;
 
 public class Step {
+
+    int x,y;
+
+    Step prevStep;
+
+
+    public Step(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.prevStep = null;
+    }
+
+    public Step(int x, int y, Step prevStep){
+        this.x = x;
+        this.y = y;
+        this.prevStep = prevStep;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Step getPrevStep() {
+        return prevStep;
+    }
 }
